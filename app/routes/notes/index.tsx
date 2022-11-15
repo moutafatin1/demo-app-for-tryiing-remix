dbd76f4473396ba148fb804e2d1cd5247434c73e
Oussama Moutafatin <moutafatin3@gmail.com>
2022-11-15 11:22:21 +0100

    fetch and display notes from json server

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -1,10 +1,46 @@
+import { useLoaderData } from "@remix-run/react";
+
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+};
+
+export async function loader() {
+  const response = await fetch("http://localhost:3004/notes");
+  const notes = (await response.json()) as Note[];
+
+  return notes;
+}
+
 const NotesIndex = () => {
+  const notes = useLoaderData<typeof loader>();
+
   return (
     <>
-      <h1 className="text-center py-4 text-4xl font-bold text-white">
-        You don't have any notes
-      </h1>
-      <img src="/undraw_taking_notes_re_bnaf.svg" alt="" />
+      {notes && (
+        <ul>
+          {notes.map((note) => (
+            <li
+              key={note.id}
+              className="flex flex-col gap-4 p-4 bg-purple-500 text-slate-50 w-64 rounded-xl"
+            >
+              <span className="text-center text-lg font-bold">
+                {note.title}
+              </span>
+              <span>{note.content}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+      {notes.length === 0 && (
+        <div>
+          <h1 className="text-center py-4 text-4xl font-bold text-white">
+            You don't have any notes
+          </h1>
+          <img src="/undraw_taking_notes_re_bnaf.svg" alt="" />
+        </div>
+      )}
     </>
   );
 };
